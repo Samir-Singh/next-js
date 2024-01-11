df859c896dc8cdeb866ae6f498171ea60e3dfba1
Samir Singh <samir.singh@innovationm.com>
2024-01-11 22:04:40 +0530

    usePathname hook introduced to get pathname

--- app/login/layout.js
+++ app/login/layout.js
@@ -1,27 +1,35 @@
+"use client";
 import { Inter } from "next/font/google";
 import style from "./login.module.css";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function LoginLayout({ children }) {
+  const pathname = usePathname();
   return (
     <html lang="en">
       <body className={inter.className}>
-        <ul className={style.ul_tag}>
-          <li className={style.li_tag}>
-            <h1>Login Layout</h1>
-          </li>
-          <li className={style.li_tag}>
-            <Link href="/login">Login Page</Link>
-          </li>
-          <li className={style.li_tag}>
-            <Link href="/login/loginStudent">Student Login</Link>
-          </li>
-          <li className={style.li_tag}>
-            <Link href="/login/loginTeacher">Teacher Login</Link>
-          </li>
-        </ul>
+        {pathname !== "/login/loginNoHeader" && (
+          <ul className={style.ul_tag}>
+            <li className={style.li_tag}>
+              <h1>Login Layout</h1>
+            </li>
+            <li className={style.li_tag}>
+              <Link href="/login">Login Page</Link>
+            </li>
+            <li className={style.li_tag}>
+              <Link href="/login/loginStudent">Student Login</Link>
+            </li>
+            <li className={style.li_tag}>
+              <Link href="/login/loginTeacher">Teacher Login</Link>
+            </li>
+            <li className={style.li_tag}>
+              <Link href="/login/loginNoHeader">No Header Login</Link>
+            </li>
+          </ul>
+        )}
         {children}
       </body>
     </html>
